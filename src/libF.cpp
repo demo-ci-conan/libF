722ca0da2efd58cdf8eec5d919352457968969dd
Niels van der Schans <Niels.vanderSchans@tomtom.com>
2019-11-20 17:06:13 +0100

    Set dependencies

--- src/libF.cpp
+++ src/libF.cpp
@@ -2,9 +2,7 @@
 #include "libF/libF.h"
 
 #include <iostream>
-#include "libA/libA.h"
 
 void hello_libF(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libF: " << msg << std::endl;
-    hello_libA(indent+1, "hello from libF");
 }
